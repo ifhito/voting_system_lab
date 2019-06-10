7f5803682a1d64363796d3ea223a94609584e779
Glitch (kkk-votingsystem) <none>
2019-06-10 15:29:06 +0000

    🏛❄️ Checkpoint ./vote.html:4674454/6 ./vote_script.js:4674454/1397

--- vote_script.js
+++ vote_script.js
@@ -25,6 +25,47 @@ function f_drop(event){
 function submit(){
   //var p_elem = document.getElementsByClassName("P_box");
   //console.log(p_elem)
-  var p_1 = document.getElementsByClassName("P_box")
-  if()
+  var p_1 = document.getElementsByClassName("p_1");
+  var p_2 = document.getElementsByClassName("p_2");
+  var p_3 = document.getElementsByClassName("p_3");
+  var p_4 = document.getElementsByClassName("p_4");
+  var fg_1 = document.getElementsByClassName("fg_1");
+  var fg_2 = document.getElementsByClassName("fg_2");
+  var fg_3 = document.getElementsByClassName("fg_3");
+  var fg_4 = document.getElementsByClassName("fg_4");
+  
+  var list = ["小山","持田","姚","加瀬","関","張","前本","池田","坂元","荻野"];
+  
+  if(list.indexOf(p_1) == -1){
+    alert("リストに存在しない名前です。")
+    return false;
+  }
+  if(list.indexOf(p_2) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
+  if(list.indexOf(p_3) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
+  if(list.indexOf(p_4) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
+  if(list.indexOf(fg_1) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
+  if(list.indexOf(fg_2) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
+  if(list.indexOf(fg_3) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
+  if(list.indexOf(fg_4) == -1){
+    alert("リストに存在しない名前です。")
+    return;
+  }
 }
