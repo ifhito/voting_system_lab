3c40efeb9731611ff6574089e0949a8b70883bec
Glitch (kkk-votingsystem) <none>
2019-06-18 01:24:38 +0000

    🌺👨‍❤️‍💋‍👨 Checkpoint ./index.html:4674454/140 ./vote_script.js:4674454/1847

--- vote_script.js
+++ vote_script.js
@@ -1,30 +1,29 @@
 var flag = 0;
 
-function submit(){
-  //var p_elem = document.getElementsByClassName("P_box");
-  //console.log(p_elem)
+var p_1 = document.getElementById("p_1").value;
+var p_2 = document.getElementById("p_2").value;
+var p_3 = document.getElementById("p_3").value;
+var p_4 = document.getElementById("p_4").value;
+var fg_1 = document.getElementById("fg_1").value;
+var fg_2 = document.getElementById("fg_2").value;
+var fg_3 = document.getElementById("fg_3").value;
+var fg_4 = document.getElementById("fg_4").value;
+
+var list = ['ghita','ギータ','ぎーた',
+            '小山','koyama','こやま',
+            '持田','mochida','もちだ',
+            '姚','you','よう',
+            '加瀬','kase','かせ',
+            '関','seki','せき',
+            '張','chou','ちょう',
+            '前本','maemoto','まえもと',
+            '池田','ikeda','いけだ',
+            '坂元','sakamoto','さかもと',
+            '荻野','ogino','おぎの'];
+
+function submit_p1(){
   var p_1 = document.getElementById("p_1").value;
-  var p_2 = document.getElementById("p_2").value;
-  var p_3 = document.getElementById("p_3").value;
-  var p_4 = document.getElementById("p_4").value;
-  var fg_1 = document.getElementById("fg_1").value;
-  var fg_2 = document.getElementById("fg_2").value;
-  var fg_3 = document.getElementById("fg_3").value;
-  var fg_4 = document.getElementById("fg_4").value;
-  
-  var list = ['ghita','ギータ','ぎーた',
-              '小山','koyama','こやま',
-              '持田','mochida','もちだ',
-              '姚','you','よう',
-              '加瀬','kase','かせ',
-              '関','seki','せき',
-              '張','chou','ちょう',
-              '前本','maemoto','まえもと',
-              '池田','ikeda','いけだ',
-              '坂元','sakamoto','さかもと',
-              '荻野','ogino','おぎの'];
-    
-  
+      
   if(list.indexOf(p_1) >= 0){
     flag += 1;
     return true;
@@ -37,7 +36,7 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(p_2 + 'はリストに存在しません.');
     return false;
   }
   
@@ -45,7 +44,7 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(p_3 + 'はリストに存在しません.');
     return false;
   }
   
@@ -53,7 +52,7 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(p_4 + 'はリストに存在しません.');
     return false;
   }
   
@@ -61,7 +60,7 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(fg_1 + 'はリストに存在しません.');
     return false;
   }
   
@@ -69,7 +68,7 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(fg_2 + 'はリストに存在しません.');
     return false;
   }
   
@@ -77,7 +76,7 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(fg_3 + 'はリストに存在しません.');
     return false;
   }
   
@@ -85,11 +84,8 @@ function submit(){
     flag += 1;
     return true;
   }else{
-    alert(p_1 + 'はリストに存在しません.');
+    alert(fg_4 + 'はリストに存在しません.');
     return false;
   }
   
-  if(flag == 8){
-    document.send.elements[0].disabled = false;
-  }
 }
